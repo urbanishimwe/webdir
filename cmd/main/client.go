1c96b153e133f324fe89a13053583cfba15a34da
Urban ISHIMWE <urbanishimwe1@gmail.com>
2024-10-05 13:43:55 +0200

    HTTP client added login

--- cmd/main/client.go
+++ cmd/main/client.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	_ "embed"
+	"encoding/hex"
 	"encoding/json"
 	"io"
 	"log"
@@ -10,6 +12,8 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/urbanishimwe/webdir/node"
 )
@@ -17,13 +21,19 @@ import (
 //go:embed index.html
 var indexHtml string
 
+//go:embed login.html
+var loginHtml string
+
+const oauthCookieName = "access-token"
+
 type httpServer struct {
-	node       *node.NodeConfig
-	httpServer net.Listener
+	node           *node.NodeConfig
+	httpServer     net.Listener
+	clientPassword string
 }
 
-func mustNewHttpServer(node *node.NodeConfig, addr string) *httpServer {
-	srv := &httpServer{node: node}
+func mustNewHttpServer(addr string) *httpServer {
+	srv := &httpServer{}
 	httpServer, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to start listen on ")
@@ -36,35 +46,110 @@ func (srv *httpServer) listenAndServe() {
 	mux := http.NewServeMux()
 
 	// THIS IS A SPECIAL ROUTE THAT IS ONLY USED BY NODES TO COMMUNICATE WITH EACH OTHER
-	mux.HandleFunc("/webdir", srv.webDirhandler)
-	// ENDS OF NODES SPECIAL ROUTES
+	mux.HandleFunc("/webdir", srv.webDirHandler)
 
-	mux.HandleFunc("/record", srv.recordHandler)
-	mux.HandleFunc("/dir", srv.dirHandler)
-	mux.HandleFunc("/nodes", srv.nodesHandler)
-	mux.HandleFunc("/ping", srv.recordHandler)
-	mux.HandleFunc("/file", srv.fileHandler)
-	mux.HandleFunc("/stop", srv.stopHandler)
-
+	mux.HandleFunc("/login", srv.loginHandler)
 	mux.HandleFunc("/", srv.homeHandler)
+
+	// ROUTES THAT NEEDS OAUTH
+	mux.HandleFunc("/record", srv.oauthFirst(srv.recordHandler, http.MethodGet))
+	mux.HandleFunc("/dir", srv.oauthFirst(srv.dirHandler, http.MethodGet))
+	mux.HandleFunc("/nodes", srv.oauthFirst(srv.nodesHandler, http.MethodGet))
+	mux.HandleFunc("/ping", srv.oauthFirst(srv.recordHandler, http.MethodGet))
+	mux.HandleFunc("/file", srv.oauthFirst(srv.fileHandler, http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete))
+	mux.HandleFunc("/stop", srv.oauthFirst(srv.stopHandler, http.MethodGet))
+	// END OF ROUTES ThAT NEEDS OAUTH
+
 	log.Printf("Node(%s) HTTP listening on: %s", srv.node.Node.Oauth.UserName, srv.httpServer.Addr())
 	http.Serve(srv.httpServer, mux)
 }
 
-func (srv *httpServer) homeHandler(wr http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet && (r.URL.Path == "" || r.URL.Path == "/") {
-		wr.Write([]byte(indexHtml))
-		return
+func (srv *httpServer) oauthFirst(h http.HandlerFunc, allowedMethod ...string) http.HandlerFunc {
+	return func(wr http.ResponseWriter, r *http.Request) {
+		if !checkAllowedMethod(r.Method, allowedMethod) {
+			wr.WriteHeader(http.StatusMethodNotAllowed)
+			wr.Header().Set("Allow", strings.Join(allowedMethod, ", "))
+			return
+		}
+
+		if srv.clientPassword == "" || srv.verifyCookie(r) {
+			h(wr, r)
+			return
+		}
+
+		wr.WriteHeader(http.StatusUnauthorized)
 	}
-	http.NotFound(wr, r)
 }
 
-func (srv *httpServer) recordHandler(wr http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+func (srv *httpServer) loginHandler(wr http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		cookie := http.Cookie{
+			Name:   oauthCookieName,
+			MaxAge: -1,
+		}
+		// logout the client as well
+		http.SetCookie(wr, &cookie)
+		wr.Write([]byte(loginHtml))
+		return
+	}
+
+	if r.Method != http.MethodPost {
+		wr.WriteHeader(http.StatusMethodNotAllowed)
+		wr.Header().Set("Allow", strings.Join([]string{http.MethodGet, http.MethodPost}, ", "))
+		return
+	}
+
+	// Go doesn't populate json body into r.FormValue after calling r.ParseForm...
+	// make it easy and add password directly inside the body
+	password, err := io.ReadAll(http.MaxBytesReader(wr, r.Body, 40))
+	if err != nil {
+		wr.WriteHeader(http.StatusBadRequest)
+		wr.Write([]byte(err.Error()))
+		return
+	}
+
+	if !bytes.Equal([]byte(srv.clientPassword), password) {
+		wr.WriteHeader(http.StatusBadRequest)
+		wr.Write([]byte("Wrong password!"))
+		return
+	}
+
+	maxAge := 24 * 60 * 60 // 1 day seconds
+	expiresAt := time.Now().Add(time.Second * time.Duration(maxAge))
+	cookie := http.Cookie{
+		Name:     oauthCookieName,
+		Value:    srv.signCookie(expiresAt),
+		MaxAge:   maxAge, // expires after 1 day
+		SameSite: http.SameSiteStrictMode,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(wr, &cookie)
+}
+
+func (srv *httpServer) homeHandler(wr http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "", "/":
+	default:
 		http.NotFound(wr, r)
 		return
 	}
 
+	if r.Method != http.MethodGet {
+		wr.WriteHeader(http.StatusMethodNotAllowed)
+		wr.Header().Set("Allow", http.MethodGet)
+		return
+	}
+
+	if srv.clientPassword == "" || srv.verifyCookie(r) {
+		wr.Write([]byte(indexHtml))
+		return
+	}
+
+	srv.loginHandler(wr, r)
+}
+
+func (srv *httpServer) recordHandler(wr http.ResponseWriter, r *http.Request) {
 	var resBody []byte
 	mssg := srv.node.ClientRecord()
 	if mssg.Status != node.StatusOk {
@@ -76,11 +161,6 @@ func (srv *httpServer) recordHandler(wr http.ResponseWriter, r *http.Request) {
 }
 
 func (srv *httpServer) dirHandler(wr http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.NotFound(wr, r)
-		return
-	}
-
 	var resBody []byte
 	mssg := srv.node.ClientDir()
 	if mssg.Status != node.StatusOk {
@@ -92,11 +172,6 @@ func (srv *httpServer) dirHandler(wr http.ResponseWriter, r *http.Request) {
 }
 
 func (srv *httpServer) nodesHandler(wr http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.NotFound(wr, r)
-		return
-	}
-
 	var resBody []byte
 	mssg := srv.node.ClientNodes()
 	if mssg.Status != node.StatusOk {
@@ -112,24 +187,21 @@ func (srv *httpServer) fileHandler(wr http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodGet:
-		resBody, _ = json.Marshal(srv.node.ClientReadFile(r.URL.Query().Get("file")))
+		resBody, _ = json.Marshal(srv.node.ClientReadFile(r.URL.Query().Get("name")))
 
 	case http.MethodPost:
-		resBody, _ = json.Marshal(srv.node.ClientCreateFile(r.URL.Query().Get("file")))
+		resBody, _ = json.Marshal(srv.node.ClientCreateFile(r.URL.Query().Get("name")))
 
 	case http.MethodPut, http.MethodPatch:
-		reqBody, _ := io.ReadAll(r.Body)
+		reqBody, _ := io.ReadAll(http.MaxBytesReader(wr, r.Body, 1<<20))
 		updateCont := node.UpdateFileContent{
-			Name:    r.URL.Query().Get("file"),
+			Name:    r.URL.Query().Get("name"),
 			Content: string(reqBody),
 		}
 		resBody, _ = json.Marshal(srv.node.ClientUpdateFile(updateCont))
 
 	case http.MethodDelete:
-		resBody, _ = json.Marshal(srv.node.ClientDeleteFile(r.URL.Query().Get("file")))
-
-	default:
-		http.NotFound(wr, r)
+		resBody, _ = json.Marshal(srv.node.ClientDeleteFile(r.URL.Query().Get("name")))
 	}
 
 	wr.Write(resBody)
@@ -137,22 +209,21 @@ func (srv *httpServer) fileHandler(wr http.ResponseWriter, r *http.Request) {
 
 func (srv *httpServer) stopHandler(wr http.ResponseWriter, r *http.Request) {
 	defer srv.httpServer.Close()
-	if r.Method != http.MethodGet {
-		http.NotFound(wr, r)
-		return
-	}
 	srv.node.Stop()
-	wr.Write(nil)
+	wr.Write([]byte("OK"))
+	if fl, ok := wr.(http.Flusher); ok {
+		fl.Flush()
+	}
 }
 
-func (srv *httpServer) webDirhandler(wr http.ResponseWriter, r *http.Request) {
+func (srv *httpServer) webDirHandler(wr http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		wr.WriteHeader(http.StatusMethodNotAllowed)
 		wr.Write(webDirFormatBadRequest("Method Not Allowed"))
 		return
 	}
 
-	reqBody, _ := io.ReadAll(r.Body)
+	reqBody, _ := io.ReadAll(http.MaxBytesReader(wr, r.Body, 1<<20))
 	var mssg node.Message
 	err := json.Unmarshal(reqBody, &mssg)
 	if err != nil {
@@ -203,4 +274,66 @@ func webDirMakeHTTPRequest(address string, mssg *node.Message) (*node.Message, e
 	var resMssg node.Message
 	err = json.Unmarshal(resRaw, &resMssg)
 	return &resMssg, err
+}
+
+func checkAllowedMethod(method string, allowed []string) bool {
+	if len(allowed) == 0 {
+		return true
+	}
+	for _, v := range allowed {
+		if v == method {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+THIS IS THE EASIEST YET SECURELY STRONG OAUTHENTICATION MECHANISM
+
+SIGNING: After verifying password
+
+* expiresAt = time.Now().Add(1 day). The expiry time of the token
+
+* shaed = sha256(expiresAt, httpPassword). httpPassword is the checksum
+
+* return hex(shaed) + " " + str(expiresAt)
+
+VERIFYING:
+
+* separate hex(shaed) and str(expiresAt) from "clientCookie"
+
+* check if expiresAt has not expired
+
+* ourCookie = signCookie(expiresAt)
+
+* return ourCookie == clientCookie
+*/
+func (h *httpServer) signCookie(expiresAt time.Time) string {
+	// - Hash the expires time with our password
+	// You will know why in verifyCookie
+	expiresAtStr := expiresAt.Format(time.RFC3339Nano)
+	signer := sha256.New()
+	signer.Write([]byte(expiresAtStr))
+	shaed := hex.EncodeToString(signer.Sum([]byte(h.clientPassword)))
+	return string(shaed) + " " + expiresAtStr
+}
+
+func (h *httpServer) verifyCookie(r *http.Request) bool {
+	c, _ := r.Cookie(oauthCookieName)
+	if c == nil {
+		return false
+	}
+
+	cookie := c.Value
+	var expiresAt time.Time
+	var err error
+
+	i := (strings.IndexByte(cookie, ' ') + 1) % len(cookie) // avoid overflow attacks
+	expiresAt, err = time.Parse(time.RFC3339Nano, cookie[i:])
+	if err != nil || expiresAt.Before(time.Now()) {
+		return false
+	}
+
+	return bytes.Equal([]byte(h.signCookie(expiresAt)), []byte(cookie))
 }
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	addr, mesh, publicAddr, username, password string
+	addr, mesh, publicAddr, username, password, httpPassword string
 )
 
 func init() {
@@ -18,24 +18,19 @@ func init() {
 	flag.StringVar(&publicAddr, "public-addr", "", "Internet address for this network if not specified node address is used instead")
 	flag.StringVar(&username, "name", "", "username of the node, if empty random text are used")
 	flag.StringVar(&password, "password", "", "password of the node, if empty random text are used")
+	flag.StringVar(&httpPassword, "http-password", "", "password for the client")
 	flag.Parse()
 }
 
 func main() {
-	temp := buildTempNodeConfig()
-	httpSrv := mustNewHttpServer(nil, addr)
-	if publicAddr == "" {
-		temp.PublicAddr = httpSrv.httpServer.Addr()
-	} else if netAddr, err := net.ResolveTCPAddr("", publicAddr); err != nil {
-		log.Fatalf("Resovling public address(%s) failed: %q", publicAddr, err)
-	} else {
-		temp.PublicAddr = netAddr
-	}
+	httpSrv := mustNewHttpServer(addr)
+	temp := buildTempNodeConfig(httpSrv)
 	httpSrv.node = node.MustInitServer(temp, mesh, webDirMakeHTTPRequest)
+	httpSrv.clientPassword = httpPassword
 	httpSrv.listenAndServe()
 }
 
-func buildTempNodeConfig() node.NodeConfig {
+func buildTempNodeConfig(srv *httpServer) node.NodeConfig {
 	tempConfig := node.NodeConfig{}
 	if username != "" {
 		tempConfig.Node.Oauth.UserName = username
@@ -43,6 +38,14 @@ func buildTempNodeConfig() node.NodeConfig {
 
 	if password != "" {
 		tempConfig.Node.Oauth.Password = password
+	}
+
+	if publicAddr == "" {
+		tempConfig.PublicAddr = srv.httpServer.Addr()
+	} else if netAddr, err := net.ResolveTCPAddr("", publicAddr); err != nil {
+		log.Fatalf("Resovling public address(%s) failed: %q", publicAddr, err)
+	} else {
+		tempConfig.PublicAddr = netAddr
 	}
 
 	return tempConfig
